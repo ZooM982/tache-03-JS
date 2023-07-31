750e2dc852bd4aebc18fb0aae69d2f963928849d
ZooM982 <134058804+ZooM982@users.noreply.github.com>
2023-07-31 16:16:22 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var age = prompt("Entrez votre age: ");
+if( age > 0 & age <= 14){
+    document.write('votre age est de ' + age + '\nans et  vous etes un Enfant');
+}
+if( age > 15 & age <= 24){
+    document.write('votre age est de ' + age + '\nans et  vous etes un Adolescent');
+}
+if( age > 25 & age <= 64){
+    document.write('votre age est de ' + age + '\nans et  vous etes un Adulte');
+}
+if( age > 65){
+    document.write('votre age est de ' + age + '\nans et  vous etes un Ainés');
+}
