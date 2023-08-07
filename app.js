796cffa2a0beb66d92847dd7c3286929423bd5a1
ZooM982 <haurlyroll@gmail.com>
2023-08-07 14:39:19 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var age = prompt("Entrez votre age: ");
-if( age > 0 & age <= 14){
+if( age >= 0 && age <= 14){
     document.write('votre age est de ' + age + '\nans et  vous etes un Enfant');
-}
-if( age > 15 & age <= 24){
+};
+if( age >= 15 && age <= 24){
     document.write('votre age est de ' + age + '\nans et  vous etes un Adolescent');
-}
-if( age > 25 & age <= 64){
+};
+if( age >= 25 && age <= 64){
     document.write('votre age est de ' + age + '\nans et  vous etes un Adulte');
-}
-if( age > 65){
+};
+if( age >= 65){
     document.write('votre age est de ' + age + '\nans et  vous etes un Ainés');
-}
+};
